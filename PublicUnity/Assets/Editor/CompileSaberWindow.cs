c8345e9ba24ac03cd832d255cc5212d3a7cffbf2
lollpdesu <lollpwillpsnacc@gmail.com>
2024-07-10 19:36:12 +0200

    Custom Saber Project
    The project was originally made by Mdot. I just cleaned it up and added some basic shaders. To use this project, you need Unity version 2019.3.15f1.

---
+++ PublicUnity/Assets/Editor/CompileSaberWindow.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class CompileSaberWindow : EditorWindow
+{
+    private SaberDescriptor[] sabers;
+
+    [MenuItem("Window/Saber Exporter")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CompileSaberWindow), false, "Saber Exporter");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Sabers", EditorStyles.boldLabel);
+        GUILayout.Space(20);
+
+        foreach (SaberDescriptor saber in sabers)
+        {
+            GUILayout.Label("GameObject : " + saber.gameObject.name, EditorStyles.boldLabel);
+            saber.AuthorName = EditorGUILayout.TextField("Author name", saber.AuthorName);
+            saber.SaberName = EditorGUILayout.TextField("Saber name", saber.SaberName);
+            saber.CoverImage = (Sprite)EditorGUILayout.ObjectField("Cover Image", saber.CoverImage, typeof(Sprite), false);
+
+            EditorGUI.BeginDisabledGroup(saber.transform.Find("LeftSaber") == null || saber.transform.Find("RightSaber") == null);
+            if (GUILayout.Button("Export " + saber.SaberName))
+            {
+                GameObject saberObject = saber.gameObject;
+                if (saberObject != null && saber != null)
+                {
+                    string path = EditorUtility.SaveFilePanel("Save saber file", "", saber.SaberName + ".saber", "saber");
+                    Debug.Log($"Is path empty?: ({string.IsNullOrWhiteSpace(path)})");
+
+                    if (!string.IsNullOrWhiteSpace(path))
+                    {
+                        string fileName = Path.GetFileName(path);
+                        string folderPath = Path.GetDirectoryName(path);
+
+                        Selection.activeObject = saberObject;
+                        EditorUtility.SetDirty(saber);
+                        EditorSceneManager.MarkSceneDirty(saberObject.scene);
+                        EditorSceneManager.SaveScene(saberObject.scene);
+                        PrefabUtility.CreatePrefab("Assets/_CustomSaber.prefab", Selection.activeObject as GameObject);
+                        AssetBundleBuild assetBundleBuild = default(AssetBundleBuild);
+                        assetBundleBuild.assetNames = new string[] {
+                            "Assets/_CustomSaber.prefab"
+                        };
+
+                        assetBundleBuild.assetBundleName = fileName;
+
+                        BuildTargetGroup selectedBuildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+                        BuildTarget activeBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+
+                        BuildPipeline.BuildAssetBundles(Application.temporaryCachePath, new AssetBundleBuild[] { assetBundleBuild }, 0, EditorUserBuildSettings.activeBuildTarget);
+                        EditorPrefs.SetString("currentBuildingAssetBundlePath", folderPath);
+                        EditorUserBuildSettings.SwitchActiveBuildTarget(selectedBuildTargetGroup, activeBuildTarget);
+                        AssetDatabase.DeleteAsset("Assets/_CustomSaber.prefab");
+
+                        if (File.Exists(path))
+                        {
+                            bool isDirectory = (File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory;
+                            if (!isDirectory)
+                            {
+                                File.Delete(path);
+                            }
+                        }
+
+                        File.Move(Path.Combine(Application.temporaryCachePath, fileName), path);
+                        AssetDatabase.Refresh();
+                        EditorUtility.DisplayDialog("Export Successful!", "Export Successful!", "OK");
+                    }
+                    else
+                    {
+                        EditorUtility.DisplayDialog("Export Failed!", "Path is invalid.", "OK");
+                    }
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("Export Failed!", "Saber GameObject is missing.", "OK");
+                }
+            }
+
+            EditorGUI.EndDisabledGroup();
+
+            if (saber.transform.Find("LeftSaber") == null)
+            {
+                GUILayout.Label("LeftSaber gameObject is missing", EditorStyles.boldLabel);
+            }
+
+            if (saber.transform.Find("RightSaber") == null)
+            {
+                GUILayout.Label("RightSaber gameObject is missing", EditorStyles.boldLabel);
+            }
+
+            if (GetObjectBounds(saber.gameObject).extents.z * 2.0 > 2.0)
+            {
+                GUILayout.Label("The saber might be too long", EditorStyles.boldLabel);
+            }
+
+            if (GetObjectBounds(saber.gameObject).extents.x * 2.0 > 1.0)
+            {
+                GUILayout.Label("The saber might be too large", EditorStyles.boldLabel);
+            }
+
+            GUILayout.Space(20);
+        }
+    }
+
+    private void OnFocus()
+    {
+        sabers = GameObject.FindObjectsOfType<SaberDescriptor>();
+    }
+
+    private Bounds GetObjectBounds(GameObject gameObject)
+    {
+        Bounds bounds = new Bounds(gameObject.transform.position, Vector3.zero);
+        foreach (Renderer renderer in gameObject.GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        return bounds;
+    }
+}
---
+++ PublicUnity/Assets/Shaders/bs_additive_desaturate.shader
@@ -0,0 +1,97 @@
+﻿// Unity built-in shader source. Copyright (c) 2016 Unity Technologies. MIT license (see license.txt)
+
+Shader "BeatSaber/Additive Desaturation" {
+Properties {
+    _Color ("Color", Color) = (1,1,1,1)
+	[MaterialToggle] _CustomColors("Custom Colors", Float) = 0
+    _Fraction ("Desaturation", Range(0.0,1.0)) = 0
+    _MainTex ("Particle Texture", 2D) = "white" {}
+    _Intensity ("Intensity", float) = 1
+    _InvFade ("Soft Particles Factor", Range(0.01,3.0)) = 1.0
+}
+
+Category {
+    Tags { "Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent" "PreviewType"="Plane" }
+    Blend SrcAlpha One
+    ColorMask RGB
+    Cull Off Lighting Off ZWrite Off
+
+    SubShader {
+        Pass {
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+            #pragma multi_compile_particles
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            fixed4 _Color;
+            float _Fraction;
+
+            struct appdata_t {
+                float4 vertex : POSITION;
+                fixed4 color : COLOR;
+                float2 texcoord : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f {
+                float4 vertex : SV_POSITION;
+                fixed4 color : COLOR;
+                float2 texcoord : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                #ifdef SOFTPARTICLES_ON
+                float4 projPos : TEXCOORD2;
+                #endif
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            float4 _MainTex_ST;
+
+            v2f vert (appdata_t v)
+            {
+                v2f o;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                #ifdef SOFTPARTICLES_ON
+                o.projPos = ComputeScreenPos (o.vertex);
+                COMPUTE_EYEDEPTH(o.projPos.z);
+                #endif
+                o.color = v.color;
+                o.texcoord = TRANSFORM_TEX(v.texcoord,_MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            UNITY_DECLARE_DEPTH_TEXTURE(_CameraDepthTexture);
+            float _InvFade;
+            float _Intensity;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                #ifdef SOFTPARTICLES_ON
+                float sceneZ = LinearEyeDepth (SAMPLE_DEPTH_TEXTURE_PROJ(_CameraDepthTexture, UNITY_PROJ_COORD(i.projPos)));
+                float partZ = i.projPos.z;
+                float fade = saturate (_InvFade * (sceneZ-partZ));
+                i.color.a *= fade;
+                #endif
+                float3 desaturateInitialColor3 = ( _Color * tex2D(_MainTex, i.texcoord)).rgb * _Intensity;
+				float desaturateDot3 = dot( desaturateInitialColor3, float3( 0.299, 0.587, 0.114 ));
+				float3 desaturateVar3 = lerp( desaturateInitialColor3, desaturateDot3.xxx, _Fraction);
+                
+                float4 col = 2.0f * i.color * float4( desaturateVar3, (_Color.a * tex2D(_MainTex, i.texcoord).a));
+                col.a = saturate(col.a); // alpha should not have double-brightness applied to it, but we can't fix that legacy behavior without breaking everyone's effects, so instead clamp the output to get sensible HDR behavior (case 967476)
+
+                UNITY_APPLY_FOG_COLOR(i.fogCoord, col, fixed4(0,0,0,0)); // fog towards black due to our blend mode
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
+}
